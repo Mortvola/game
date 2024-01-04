1a740fd75fa851bf9e88baff93430c08afb80d7f
Mortvola <mortvola@gmail.com>
2024-01-04 06:57:59 -0700

    Completed Poison Spray.

--- src/Character/Actions/Spells/PoisonSpray.ts
+++ src/Character/Actions/Spells/PoisonSpray.ts
@@ -1,9 +1,21 @@
+import { diceRoll, savingThrow } from "../../../Dice";
 import { feetToMeters } from "../../../Math";
-import Spell from "./Spell";
+import Script from "../../../Script/Script";
+import { WorldInterface } from "../../../WorldInterface";
+import Actor from "../../Actor";
+import RangeSpell from "./RangeSpell";
 
-class PoisonSpray extends Spell {
+class PoisonSpray extends RangeSpell {
   constructor() {
-    super('Poison Spray', 'Action', 0, feetToMeters(10), 0, false);
+    super(1, true, 'Poison Spray', 'Action', 0, feetToMeters(10), 0, false);
+  }
+
+  cast(actor: Actor, script: Script, world: WorldInterface) {
+    const st = savingThrow(this.targets[0].character, this.targets[0].character.abilityScores.constitution, 'Neutral');
+
+    if (st < actor.character.spellCastingDc) {
+      this.targets[0].takeDamage(diceRoll(1, 12), false, actor, this.name, script);
+    }
   }
 }
 
